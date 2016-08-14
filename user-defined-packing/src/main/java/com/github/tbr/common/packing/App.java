f925d4dde633f3a16c0373ca34bd3c7dab79d5b0
none <none>
2016-08-14 22:29:12 +0800

    First commit all to git

---
+++ user-defined-packing/src/main/java/com/github/tbr/common/packing/App.java
@@ -0,0 +1,13 @@
+package com.github.tbr.common.packing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
